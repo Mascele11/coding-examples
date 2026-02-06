72dbc8dbdaecd041b0ef2b2a6fbe4c3e5d8fb9de
mb <marcello.babbi@gmail.com>
2026-02-06 18:01:32 +0100

    Modified DatasetLoader descriptions

--- ml-model-dev-project-example/package/preprocessing/pytorch_dataloader.py
+++ ml-model-dev-project-example/package/preprocessing/pytorch_dataloader.py
@@ -5,26 +5,36 @@ import pickle
 import numpy as np
 import pandas as pd
 from torch_geometric.utils import dense_to_sparse
+from torch_geometric_temporal.dataset import METRLADatasetLoader
 from torch_geometric_temporal.signal import StaticGraphTemporalSignal
 
 class DatasetLoader(object):
-    """A price forecasting dataset for ERCOT electric network. The dataset contains the LMP-Real Time collected each 15
-    minutes for all the nodes provided. It takes input the adjency matrix of the graph the real-tine features.
+    """A DatasetLoader class to generate a StaticGraphTemporalSignal dataset readable by pytorch_geometric_temporal
+    graph models.
 
-    adj_mat.npy = [# nodes x # nodes] {it resumes the static graph architecture}
-    node_values.npy  = [# of samples(at 15 minutes) x # nodes x # features] { 3-D vector}
-    """
+    The DatasetLoader is by default handling real-time collected each 15 minutes (sample-time) for all the nodes provided.
+    It takes input the adjency matrix of the graph the real-tine features.
 
-    def __init__(self, raw_data_dir=os.path.join(os.getcwd(), "data")):
+    Parameters
+    ----------
+        adj_mat_file_name.npy = [# nodes x # nodes] {it resumes the static graph architecture}
+        nodes_values_file_name.npy  = [# of samples(at 15 minutes) x # nodes x # features] {3-D vector}
+   ----------
+   """
+
+    def __init__(self, raw_data_dir=os.path.join(os.getcwd(), "data"), adj_mat_file_name = "A.npy",
+                 node_values_file_name = "nodes_values.npy"):
         super(DatasetLoader, self).__init__()
         self.raw_data_dir = raw_data_dir
+        self.A_name = adj_mat_file_name
+        self.node_values_file_name = node_values_file_name
         self._read_raw_data()
 
     def _read_raw_data(self):
-        A = np.load(os.path.join(self.raw_data_dir, "A.npy"))
-        X = np.load(os.path.join(self.raw_data_dir, "node_values_ercot.npy")).transpose(
-            (1, 0))  # transpose into: [#nodes x #features x #sample]
-        X = X.astype(np.float32)
+        A = np.load(os.path.join(self.raw_data_dir, self.A_name))
+        X = np.load(os.path.join(self.raw_data_dir, self.node_values_file_name)).transpose(
+            (1, 0))  # transpose into: [#nodes x #features x #sample] - Expected by StatiGraphTemporalSignal
+        X = X.astype(np.float32) # conversion for GPU compatibility
 
         """
         # Normalise as in DCRNN paper (via Z-Score Method)
@@ -55,12 +65,14 @@ class DatasetLoader(object):
             num_timesteps_in (int): number of timesteps the sequence model sees
             num_timesteps_out (int): number of timesteps the sequence model has to predict
         """
+
+        # Rolling Time Window Indices Generation: prediction is consecutive
         indices = [
             (i, i + (num_timesteps_in + num_timesteps_out))
             for i in range(self.X.shape[1] - (num_timesteps_in + num_timesteps_out) + 1)
         ]
 
-        # Generate observations
+        # Generate observations (features, target):
         features, target = [], []
         for i, j in indices:
             features.append((self.X[:, i: i + num_timesteps_in]).numpy())
@@ -70,12 +82,11 @@ class DatasetLoader(object):
         self.targets = target
 
     def get_dataset(self, num_timesteps_in: int = 12, num_timesteps_out: int = 12, save: bool = False) -> StaticGraphTemporalSignal:
-        """Returns data iterator for METR-LA dataset as an instance of the
+        """Returns data iterator for specified data as an instance of the
         static graph temporal signal class.
 
         Return types:
-            * **dataset** *(StaticGraphTemporalSignal)* - The METR-LA traffic
-                forecasting dataset.
+            * **data** *(StaticGraphTemporalSignal)* - The forecasting data (node regression).
         """
         self._get_edges_and_weights()
         self._generate_task(num_timesteps_in, num_timesteps_out)
